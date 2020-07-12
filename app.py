c26124293d42bd1d60d3c0ee26087cb5ecf1ab6e
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-07-12 12:18:24 -0700

    Added, tested word and sentence variations.

--- app.py
+++ app.py
@@ -13,17 +13,39 @@ slack_events_adapter = SlackEventAdapter(
 # Initialize a Web API client
 client = WebClient(token=os.environ["SLACK_API_TOKEN"])
 
-
 @slack_events_adapter.on("message")
 def handle_message(event_data):
-    message = event_data["event"]
+    message_for_white_list = event_data["event"]
+    message_for_black_list = event_data["event"]
+    message_for_master = event_data["event"]
+    message_for_slave = event_data["event"]
+    message_for_guys = event_data["event"]
     user = event_data["event"]["user"]
-   
-    if "whitelist" in message.get('text').lower():
-        message = "Hi <@%s>! Please use Allow List and NOT white list" % message["user"]
+    
+    if "white list" in message_for_white_list.get('text').lower() or "white-list" in message_for_white_list.get('text').lower() or "whitelist" in message_for_white_list.get('text').lower():
+        message_for_white_list = "Hi <@%s>, please use Allow List instead of white list as a preferred inclusive word." % message_for_white_list["user"]
         response = client.conversations_open(users=[user])
-        response = client.chat_postMessage(channel=response.get("channel")["id"], text=message)
-        
+        response = client.chat_postMessage(channel=response.get("channel")["id"], text=message_for_white_list)
+    
+    if "black list" in message_for_black_list.get('text').lower() or "black-list" in message_for_black_list.get('text').lower() or "blacklist" in message_for_black_list.get('text').lower():
+        message_for_black_list = "Hi <@%s>, please use Deny List instead of black list as a preferred inclusive word." % message_for_white_list["user"]
+        response = client.conversations_open(users=[user])
+        response = client.chat_postMessage(channel=response.get("channel")["id"], text=message_for_black_list)
+    
+    if "master" in message_for_master.get('text').lower():
+        message_for_master = "Hi <@%s>, please use Primary instead of master as a preferred inclusive word." % message_for_master["user"]
+        response = client.conversations_open(users=[user])
+        response = client.chat_postMessage(channel=response.get("channel")["id"], text=message_for_master)
+
+    if "slave" in message_for_slave.get('text').lower():
+        message_for_slave = "Hi <@%s>, please use Secondary instead of slave as a preferred inclusive word." % message_for_slave["user"]
+        response = client.conversations_open(users=[user])
+        response = client.chat_postMessage(channel=response.get("channel")["id"], text=message_for_slave)
+
+    if "guys" in message_for_guys.get('text').lower():
+        message_for_guys = "Hi <@%s>, please use peeps, people, y'all, folks or any other non gendered pronoun instead of guys to promote inclusion." % message_for_guys["user"]
+        response = client.conversations_open(users=[user])
+        response = client.chat_postMessage(channel=response.get("channel")["id"], text=message_for_guys)
 
 if __name__ == "__main__":
     logger = logging.getLogger()
